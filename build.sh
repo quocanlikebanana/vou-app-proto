21dec41939c60bd1b0df83a36c42af777e651a13
21120190 <quocankg2611@gmail.com>
2024-12-09 15:01:24 +0700

    Fix sh

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ protoc --plugin=$(npm root)/.bin/protoc-gen-ts_proto \
  --ts_proto_out=dist \
  --ts_proto_opt=outputServices=grpc-js \
  --ts_proto_opt=esModuleInterop=true \
- -I=src/ proto/**/*.proto
+ -I=proto proto/**/*.proto
